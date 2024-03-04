0c05ba217ed15b31c1a6cdea2abdf572b3d1bb2f
Steven Rud <99210111+StevenRud7@users.noreply.github.com>
2024-03-05 00:57:19 +0200

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -103,4 +103,4 @@ def update_info_box(click_data):
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8050)))
